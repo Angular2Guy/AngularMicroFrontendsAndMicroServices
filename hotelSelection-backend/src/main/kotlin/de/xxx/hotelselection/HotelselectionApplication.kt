5d8fd3f38febda49c872b6678fa4c66b66a6f33e
Angular2guy <angular2guy@gmail.com>
2024-10-02 13:37:01 +0200

    feat: add projects

---
+++ hotelSelection-backend/src/main/kotlin/de/xxx/hotelselection/HotelselectionApplication.kt
@@ -0,0 +1,11 @@
+package de.xxx.hotelselection
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HotelselectionApplication
+
+fun main(args: Array<String>) {
+	runApplication<HotelselectionApplication>(*args)
+}
---
+++ hotelSelection-backend/src/test/kotlin/de/xxx/hotelselection/HotelselectionApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.xxx.hotelselection
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HotelselectionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
