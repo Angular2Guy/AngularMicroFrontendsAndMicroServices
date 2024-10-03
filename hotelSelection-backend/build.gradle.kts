9bff0092489c2be172c921151cccf9c7fce964ca
Angular2guy <angular2guy@gmail.com>
2024-10-03 13:07:34 +0200

    feat: use intellij ce

--- hotelSelection-backend/build.gradle.kts
+++ hotelSelection-backend/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("com.h2database:h2:2.3.232")
 }
 
 kotlin {
---
+++ hotelSelection-backend/src/main/kotlin/de/xxx/hotelselection/adapter/controller/HotelController.kt
@@ -0,0 +1,9 @@
+package de.xxx.hotelselection.adapter.controller
+
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+
+@RestController
+@RequestMapping("rest/hotel")
+class HotelController {
+}
