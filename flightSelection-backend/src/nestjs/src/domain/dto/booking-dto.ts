a69abd8c86081ab7ac4f2ca1368f0e711d3ee5a3
Angular2guy <angular2guy@gmail.com>
2024-10-15 11:20:05 +0200

    fix: dto

--- flightSelection-backend/src/nestjs/src/domain/dto/booking-dto.ts
+++ flightSelection-backend/src/nestjs/src/domain/dto/booking-dto.ts
@@ -1,5 +1,3 @@
-import { Flight } from "../entity/flight";
-
 /**
  *    Copyright 2019 Sven Loesekann
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +10,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
  */
+import { FlightDto } from "./flight-dto";
+
 export class BookingDto { 
-    constructor(public id: string, public flightDate: Date,public flight: Flight) {}  
+    constructor(public id: string, public flightDate: Date,public flightDto: FlightDto) {}  
 }
