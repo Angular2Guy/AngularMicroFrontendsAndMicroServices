b8c14101ce94c94a30d62c0dd7062b73c89bd2c7
Angular2guy <angular2guy@gmail.com>
2024-11-08 06:57:44 +0100

    fix: port

--- payment-frontend/src/angular/src/app/booking/booking.component.ts
+++ payment-frontend/src/angular/src/app/booking/booking.component.ts
@@ -11,7 +11,7 @@
    limitations under the License.
  */
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button'; 
+import {MatButtonModule} from '@angular/material/button';
 import { Router } from '@angular/router';
 import {DomSanitizer} from '@angular/platform-browser';
 
@@ -27,12 +27,12 @@ export class BookingComponent {
 
   constructor(private router: Router, private domSanitizer: DomSanitizer) { }
 
-  protected showFlights(): void {    
+  protected showFlights(): void {
     this.iframeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('http://localhost:3000/');
   }
 
   protected showHotels(): void {
-    this.iframeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('http://localhost:8080/');
+    this.iframeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('http://localhost:8088/');
   }
 
   protected showPayment(): void {
