3defaab5a8ff76567c9b25e15a9527463204dabb
Angular2guy <angular2guy@gmail.com>
2024-11-17 17:38:28 +0100

    fix: script

--- runStructurizr.sh
+++ runStructurizr.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker pull structurizr/lite
-docker run -it --rm -p 8080:8080 -v ~/git/AngularPortfolioMgr/structurizr:/usr/local/structurizr structurizr/lite
+docker run -it --rm -p 8080:8080 -v ~/git/AngularMicroFrontendsAndMicroServices/structurizr:/usr/local/structurizr structurizr/lite
