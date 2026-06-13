18bc3ac580290d89b6929f95b76fd53769a01388
Angular2guy <angular2guy@gmail.com>
2026-06-13 04:52:47 +0200

    feat: update build

--- hotelSelection-backend/build.gradle.kts
+++ hotelSelection-backend/build.gradle.kts
@@ -13,7 +13,7 @@ limitations under the License.
 plugins {
 	kotlin("jvm") version "2.2.21"
 	kotlin("plugin.spring") version "2.2.21"
-	id("org.springframework.boot") version "4.0.0"
+	id("org.springframework.boot") version "4.1.0"
 	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("plugin.jpa") version "2.2.21"
 }
